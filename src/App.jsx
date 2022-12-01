5d737f6c12a71bae639cebe3baea5b2280744ddd
Maula Izza Azizi <72302421+storyofhis@users.noreply.github.com>
2022-12-01 23:09:56 +0700

    buggs on rendering Home

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,44 @@ import BaseScene from "./ui/BaseScene";
 import ThreeModel from "./component/ThreeModel";
 import BaseCharacter from "./ui/BaseCharacter";
 import Rumah from "./component/Home/Rumah";
+// import Home from "./component/Home/Home";
+// import House from "/Home/HouseProject";
+
+import { useState, useEffect } from "react";
+
+// time
 
 function App() {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus(null);
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
+
   return (
     <>
+      {/* <Suspense> */}
       <BaseScene>
         <BaseCharacter controls position={[0, 2, 0]} args={[0.5]} color="yellow" />
         <Rumah />
         <ambientLight args={["#ffffff", 0.25]} />
-        {/* <spotLight args={["#ffffff", 1.5, 7, angleToRadians(45), 0.4]} position={[-6, 4, 1]} castShadow /> */}
+
         <ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[10, 0, -5]} />
         <ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[0, 0, 10]} />
         <ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[-10, 0, 5]} />
@@ -25,6 +54,7 @@ function App() {
         <ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[10, 0, 5]} />
         <Sky />
       </BaseScene>
+      {/* </Suspense> */}
     </>
   );
 }
--- src/ui/BaseScene.jsx
+++ src/ui/BaseScene.jsx
@@ -5,12 +5,28 @@ import "../App.css";
 import Lights from "../component/Lights";
 import Floor from "../component/Floor";
 
+import { useState, useEffect } from "react";
+
 const BasicScene = ({ children }) => {
+  const [dateState, setDateState] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+  }, []);
   return (
     <div>
       <Canvas shadows camera={{ fov: 50 }} id="three-canvas-container">
         <Lights />
-
+        {/* {dateState.toLocaleDateString("en-GB", {
+          day: "numeric",
+          months: "short",
+          year: "numeric",
+        })}
+        <br />
+        {dateState.toLocaleDateString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: true,
+        })} */}
         <Physics gravity={[0, -9.8, 0]}>
           {children}
 
